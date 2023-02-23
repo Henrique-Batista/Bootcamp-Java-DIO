a8c65418bf5ca4a1788dc6b43047015be7d0c5a6
Pablo Henrique Batista <siriatosdravate@gmail.com>
2023-02-22 21:58:27 -0300

    API integrada ao script

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,28 @@
-console.log("teste");
+
+const pokemonOl = document.getElementById("pokemonList");
+
+function convertPokemonToLi (pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">glass</li>
+            <li class="type">poison</li>
+        </ol>
+        
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+            alt="${pokemon.name}">
+    </div>
+</li>
+`
+}
+
+pokeApi.getPokemons().then((pokemonList = []) => { 
+
+        pokemonOl.innerHTML += pokemonList.map(convertPokemonToLi).join("");
+
+    })
+    .catch((error) => console.error(error))
